be24a6b230404b003ae07049aa17a23d86807f6c
nashwallya <nashwaazp@gmail.com>
2024-11-26 22:46:59 +0700

    Perbaikan

--- app/src/main/java/com/example/w1_android/HomeActivity.kt
+++ app/src/main/java/com/example/w1_android/HomeActivity.kt
@@ -1,12 +1,18 @@
 package com.example.w1_android
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class HomeActivity : AppCompatActivity() {
+    // Logging Tag
+    private val TAG = "HomeActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,17 @@ class HomeActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
+
+        // Refer UI
+        val logoutButton = findViewById<Button>(R.id.LogoutButton)
+
+        logoutButton.setOnClickListener {
+            // Kembali ke Login Page
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Menghentikan aktivitas saat ini
+            Log.i(TAG, "Logout berhasil")
+            }
+
+        }
 }
